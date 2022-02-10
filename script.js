d4fe8e0405ae9f8fb31986b2f2b3f81b1c930898
Weslley Vieira <84158105+TLT-sptb@users.noreply.github.com>
2022-02-10 18:49:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
+
+//Inicio da animação com Scroll
 const target = document.querySelectorAll('[data-anime]')
 const animationClass = 'animate';
-const app1 = "app2"
 
 function animeScroll(){
     const windowTop = window.pageYOffset +500;
     target.forEach(function(element){
         if( (windowTop) > element.offsetTop){
             element.classList.add(animationClass);
-            element.classList.add(app1);
+            
         }
     })
 }
 window.addEventListener('scroll', function(){
     animeScroll()
 })
-
-const apps = document.querySelectorAll( "[data-scroll]");
-const teste = "app2"
-
-function Scroll(){
-    apps.forEach((app) => {
-        const sectionTOP = app.getBoundingClientRect().top +10;
-        if (sectionTOP < 0){
-            app.classList.add('teste')
-        }
-
-    })
-}
+// Fim da Animação com Scroll
 
 
-window.addEventListener('scroll', Scroll())
+
+// Inicio da animação "Escrita"
+    function typeW(elemento){
+        const hArray = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        hArray.forEach((letra, i) =>{
+            setTimeout(function (){
+                elemento.innerHTML += letra;
+            },75 * i)
+        })
+    }
+const h = document.querySelector(".h1");
+
+typeW(h);
+
+//Fim da animação Escrita
+
+
+//Cookies
+
